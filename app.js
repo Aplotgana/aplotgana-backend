8424d1e5aff3a31f112b34fbd91c6cb0817f8a89
adamrayw <raywibowo68@gmail.com>
2023-07-10 23:56:29 +0700

    change port to PORT

--- app.js
+++ app.js
@@ -31,6 +31,6 @@ app.use('/api/users', usersRouter);
 //   res.render('error');
 // });
 
-app.listen(process.env.port || 3000, function () {
-  console.log('Express server listening on port %d in %s mode', this.address().port, app.settings.env);
+app.listen(process.env.PORT || 3000, function () {
+  console.log('Server running...');
 });
